608f8bba0fdd888bd3cf113caffc8ba9be85289b
erecoliza <eduardo.recoliza@gmail.com>
2022-09-08 16:08:47 -0300

    Redes Sociales

--- portfolioapp/src/app/componentes/encabezado/encabezado.component.ts
+++ portfolioapp/src/app/componentes/encabezado/encabezado.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/servicios/portfolio.service';
 
 @Component({
   selector: 'app-encabezado',
   templateUrl: './encabezado.component.html',
   styleUrls: ['./encabezado.component.scss']
 })
-export class EncabezadoComponent implements OnInit {
-
-  constructor() { }
+export class EncabezadoComponent implements OnInit {  
+  miPortfolio:any;
+  constructor(private datosPorfolio:PortfolioService) { }
 
   ngOnInit(): void {
+    this.datosPorfolio.obtenerDatos().subscribe(data => {
+       this.miPortfolio=data;
+    });
   }
-
 }
