8a0397470da1ae2445e7e0a80c905b4c448747e3
erecoliza <eduardo.recoliza@gmail.com>
2022-09-23 12:50:30 -0300

    comence con api

--- portfolioapp/src/app/servicios/portfolioapi.service.spec.ts
+++ portfolioapp/src/app/servicios/portfolioapi.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { PortfolioService } from './portfolio.service';
+import { PortfolioapiService } from './portfolioapi.service';
 
-describe('PortfolioService', () => {
-  let service: PortfolioService;
+describe('PortfolioapiService', () => {
+  let service: PortfolioapiService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(PortfolioService);
+    service = TestBed.inject(PortfolioapiService);
   });
 
   it('should be created', () => {
